c8a7af79de0714836af06ec314aea9bda96a683e
pablommmmm <mediavillap13@gmail.com>
2025-02-12 13:07:50 +0100

    prueba3

---
+++ datos_usuario/EC2ProxyAZ2/haproxy-install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Dar permisos de ejecución al script secundario
+chmod +x duckdns-ejabberd-proxy.sh
+
+echo "Ejecutando script duckdns..."
+sudo ./duckdns-ejabberd-proxy.sh
+
+echo "Instalando HAProxy"
+sudo apt install haproxy -y
+# Mover configuración de HAProxy
+echo "Configurando HAProxy..."
+sudo mv haproxy.cfg /etc/haproxy/haproxy.cfg
+
+# Reiniciar HAProxy para aplicar los cambios
+echo "Reiniciando HAProxy..."
+sudo systemctl restart haproxy
+
+echo "Proceso completado exitosamente."
