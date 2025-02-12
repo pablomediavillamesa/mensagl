b77749a07cbb5ef76f2a3b2cd97e512c53be861d
pablommmmm <mediavillap13@gmail.com>
2025-02-12 10:01:45 +0100

    hola

--- CloudFormation/conProxy/deploy.sh
+++ CloudFormation/conProxy/deploy.sh
@@ -42,7 +42,7 @@ done
 
 echo ""
 
-# 3️ Eliminar stacks si se usa --force-redeploy
+# 3️ Eliminar stacks  de instancias si se usa --force-redeploy
 if [ "$FORCE_REDEPLOY" = true ]; then
     echo "Eliminando stacks previos..."
     aws cloudformation delete-stack --stack-name "$STACK_INSTANCES"
@@ -70,28 +70,6 @@ echo ""
 S3_FILE="Cloudformation-s3.yaml"
 STACK_S3="equipo3-s3"
 
-# 6️ Asegurar que el bucket S3 existe (no lo borra, solo lo crea si no existe)
-BUCKET_NAME="equipo3-Scripts-Bucket"
-
-if aws s3 ls "s3://$BUCKET_NAME" 2>&1 | grep -q 'NoSuchBucket'; then
-    echo "El bucket S3 no existe. Creándolo..."
-    aws cloudformation create-stack --stack-name "$STACK_S3" --template-body file://$S3_FILE --capabilities CAPABILITY_NAMED_IAM
-    aws cloudformation wait stack-create-complete --stack-name "$STACK_S3"
-    echo "Bucket S3 creado exitosamente."
-else
-    echo "El bucket S3 ya existe. Saltando la creación."
-fi
-
-echo ""
-
-# 7 Subir solo los scripts nuevos/modificados a S3
-BUCKET_NAME="equipo3-Scripts-Bucket"
-
-echo "Actualizando scripts en S3..."
-aws s3 sync ./scripts s3://$BUCKET_NAME/ --exact-timestamps
-echo "Actualización de scripts completada."
-
-echo ""
 
 
 # 5️ Crear los Security Groups
